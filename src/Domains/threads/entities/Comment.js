c406b57f2d17e5b2458862a3b84af8fb7d36f809
deanuharatinu <deanu.alt@gmail.com>
2023-06-02 23:52:30 +0700

    Add 'Comment' and test

---
+++ src/Domains/threads/entities/Comment.js
@@ -0,0 +1,24 @@
+/* eslint-disable class-methods-use-this */
+class Comment {
+  constructor(payload) {
+    this._verifyPayload(payload);
+
+    const { id, content, owner } = payload;
+
+    this.id = id;
+    this.content = content;
+    this.owner = owner;
+  }
+
+  _verifyPayload({ id, content, owner }) {
+    if (!id || !content || !owner) {
+      throw new Error('COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    }
+
+    if (typeof id !== 'string' || typeof content !== 'string' || typeof owner !== 'string') {
+      throw new Error('COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = Comment;
---
+++ src/Domains/threads/entities/_test/Comment.test.js
@@ -0,0 +1,40 @@
+const Comment = require('../Comment');
+
+describe('a Comment entities', () => {
+  it('should throw error when paylod did not contain needed property', () => {
+    // Arrange
+    const payload = {};
+
+    // Action and Assert
+    expect(() => new Comment(payload)).toThrowError('COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specs', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      content: true,
+      owner: '123',
+    };
+
+    // Action and Assert
+    expect(() => new Comment(payload)).toThrowError('COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create object correctly', () => {
+    // Arrange
+    const payload = {
+      id: '123',
+      content: 'content',
+      owner: '123',
+    };
+
+    // Action
+    const comment = new Comment(payload);
+
+    // Assert
+    expect(comment.id).toEqual(payload.id);
+    expect(comment.content).toEqual(payload.content);
+    expect(comment.owner).toEqual(payload.owner);
+  });
+});
