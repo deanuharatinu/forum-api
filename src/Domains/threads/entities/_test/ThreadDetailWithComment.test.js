cfef140b67796fc9a4d05aa0d0481380ea535c46
deanuharatinu <deanu.alt@gmail.com>
2023-06-08 03:48:53 +0700

    ThreadDetailWithComments and ThreadDetailWithoutComments

---
+++ src/Domains/threads/entities/_test/ThreadDetailWithComment.test.js
@@ -0,0 +1,49 @@
+const ThreadDetailWithComments = require('../ThreadDetailWithComments');
+
+describe('a ThreadDetailWithComments entities', () => {
+  it('should throw error when payload did not containt needed property', () => {
+    // Arrange
+    const payload = {};
+
+    // Action and Assert
+    expect(() => new ThreadDetailWithComments(payload)).toThrowError('THREAD_DETAIL_WITH_COMMENTS.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      title: 'a title',
+      body: 'a body',
+      date: 12312312,
+      username: 'username',
+      comments: [],
+    };
+
+    // Action and Assert
+    expect(() => new ThreadDetailWithComments(payload)).toThrowError('THREAD_DETAIL_WITH_COMMENTS.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create thread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: '123',
+      title: 'a title',
+      body: 'a body',
+      date: '12312312',
+      username: 'username',
+      comments: [],
+    };
+
+    // Action
+    const thread = new ThreadDetailWithComments(payload);
+
+    // Assert
+    expect(thread.id).toEqual(payload.id);
+    expect(thread.title).toEqual(payload.title);
+    expect(thread.body).toEqual(payload.body);
+    expect(thread.date).toEqual(payload.date);
+    expect(thread.username).toEqual(payload.username);
+    expect(thread.comments).toEqual(payload.comments);
+  });
+});
---
+++ src/Domains/threads/entities/_test/ThreadDetailWithoutComments.test.js
@@ -0,0 +1,46 @@
+const ThreadDetailWithoutComments = require('../ThreadDetailWithoutComments');
+
+describe('a ThreadDetailWithoutComments entities', () => {
+  it('should throw error when payload did not containt needed property', () => {
+    // Arrange
+    const payload = {};
+
+    // Action and Assert
+    expect(() => new ThreadDetailWithoutComments(payload)).toThrowError('THREAD_DETAIL_WITHOUT_COMMENTS.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      title: 'a title',
+      body: 'a body',
+      date: 12312312,
+      username: 'username',
+    };
+
+    // Action and Assert
+    expect(() => new ThreadDetailWithoutComments(payload)).toThrowError('THREAD_DETAIL_WITHOUT_COMMENTS.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create thread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: '123',
+      title: 'a title',
+      body: 'a body',
+      date: '12312312',
+      username: 'username',
+    };
+
+    // Action
+    const thread = new ThreadDetailWithoutComments(payload);
+
+    // Assert
+    expect(thread.id).toEqual(payload.id);
+    expect(thread.title).toEqual(payload.title);
+    expect(thread.body).toEqual(payload.body);
+    expect(thread.date).toEqual(payload.date);
+    expect(thread.username).toEqual(payload.username);
+  });
+});
