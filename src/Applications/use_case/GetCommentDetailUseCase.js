4545104f4faabda611ca3b6df5499aa82d1bd04b
deanuharatinu <deanu.alt@gmail.com>
2023-06-11 15:17:56 +0700

    Add GetReplyDetailUseCase

--- src/Applications/use_case/GetCommentDetailUseCase.js
+++ src/Applications/use_case/GetCommentDetailUseCase.js
@@ -1,4 +1,3 @@
-const ThreadDetailWithComments = require('../../Domains/threads/entities/ThreadDetailWithComments');
 const NotFoundError = require('../../Commons/exceptions/NotFoundError');
 
 class GetReplyDetailUseCase {
@@ -7,28 +6,12 @@ class GetReplyDetailUseCase {
   }
 
   async execute(commentId) {
-    let replies = null;
     try {
-      replies = await this._replyRepository
-        .getThreadDetailByThreadId(threadId);
+      return await this._replyRepository
+        .getRepliesByCommentId(commentId);
     } catch (error) {
       throw new NotFoundError('GET_THREAD_DETAIL_USE_CASE.THREAD_NOT_FOUND');
     }
-
-    let commentsDetail = [];
-    try {
-      commentsDetail = await this._commentRepository
-        .getCommenstByThreadId(threadId);
-    } catch (error) {
-      // ignore
-    }
-
-    const entityPayload = {
-      ...threadWithoutComments,
-      comments: commentsDetail,
-    };
-
-    return new ThreadDetailWithComments(entityPayload);
   }
 }
 
---
+++ src/Applications/use_case/_test/GetCommentDetailUseCase.test.js
@@ -0,0 +1,55 @@
+const ReplyRepository = require('../../../Domains/replies/ReplyRepository');
+const ReplyDetail = require('../../../Domains/replies/entities/ReplyDetail');
+const GetCommentDetailUseCase = require('../GetCommentDetailUseCase');
+
+describe('GetCommentDetailUseCase', () => {
+  it('should throw error when reply is not found', async () => {
+    // Arrange
+    const mockReplyRepository = new ReplyRepository();
+
+    mockReplyRepository.getRepliesByCommentId = jest.fn()
+      .mockImplementation(() => { throw new Error(); });
+
+    const getCommentDetailUseCase = new GetCommentDetailUseCase({
+      replyRepository: mockReplyRepository,
+    });
+
+    // Action and Assert
+    await expect(getCommentDetailUseCase.execute(''))
+      .rejects
+      .toThrowError('GET_THREAD_DETAIL_USE_CASE.THREAD_NOT_FOUND');
+  });
+
+  it('should return correct reply object', async () => {
+    // Arrange
+    const replies = [
+      new ReplyDetail({
+        id: '123',
+        content: 'a content',
+        date: 'date',
+        username: 'username',
+        is_deleted: false,
+      }),
+    ];
+
+    const mockReplyRepository = new ReplyRepository();
+
+    mockReplyRepository.getRepliesByCommentId = jest.fn()
+      .mockImplementation(() => Promise.resolve(replies));
+
+    const getCommentDetailUseCase = new GetCommentDetailUseCase({
+      replyRepository: mockReplyRepository,
+    });
+
+    // Action
+    const result = await getCommentDetailUseCase.execute('');
+
+    // Assert
+    expect(result.length).toEqual(1);
+    const reply = result[0];
+    expect(reply.id).toEqual('123');
+    expect(reply.content).toEqual('a content');
+    expect(reply.date).toEqual('date');
+    expect(reply.username).toEqual('username');
+  });
+});
